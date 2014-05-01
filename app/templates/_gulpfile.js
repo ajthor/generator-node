31751f59b80e0c627f0f32b645540d1d9418442b
Adam <adam.thorpe.g@gmail.com>
2014-05-01 11:23:11 -0600

    update gulpfile

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -1,7 +1,9 @@
+var fs = require('fs');
 var exec = require('child_process').exec;
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var debug = require('gulp-debug');
 
 var git = require('gulp-git');
 var github = require('github');
@@ -13,12 +15,13 @@ var remember = require('gulp-remember');
 
 
 
-gulp.task('git-init', function() {
+gulp.task('git-init', function(done) {
 	git.init();
+	done();
 });
 
 gulp.task('git-initial-commit', ['git-init'], function(done) {
-	gulp.src(['.', '!node_modules/'])
+	gulp.src(['./*', '!./node_modules/**/*.*', '!./index.html'])
 		.pipe(cache('master'))
 		.pipe(git.add())
 		.pipe(git.commit('initial commit'))
@@ -29,58 +32,47 @@ gulp.task('git-initial-commit', ['git-init'], function(done) {
 });
 
 gulp.task('init', [
-	'git-init', 
 	'git-initial-commit'
 ]);
 
 
 
-gulp.task('git-create-gh-pages', ['init'], function(done) {
-	gulp.src('.')
-		.pipe(git.checkout('--orphan gh-pages'));
-	// (function() {
-	// 	exec('git checkout --orphan gh-pages', {cwd: process.cwd()},
-	// 		function (error, stdout, stderr) {
-	// 			console.log('stdout: ' + stdout);
-	// 			console.log('stderr: ' + stderr);
-	// 			if (error !== null) {
-	// 				console.log('exec error: ' + error);
-	// 			}
-	// 		});
-	// 	exec('git rm -rf .', {cwd: process.cwd()},
-	// 		function (error, stdout, stderr) {
-	// 			console.log('stdout: ' + stdout);
-	// 			console.log('stderr: ' + stderr);
-	// 			if (error !== null) {
-	// 				console.log('exec error: ' + error);
-	// 			}
-	// 		});
-	// 	exec('echo \"Docs coming soon.\" > index.html', {cwd: process.cwd()},
-	// 		function (error, stdout, stderr) {
-	// 			console.log('stdout: ' + stdout);
-	// 			console.log('stderr: ' + stderr);
-	// 			if (error !== null) {
-	// 				console.log('exec error: ' + error);
-	// 			}
-	// 		});
-	// })();
+gulp.task('git-create-gh-pages', ['git-init'], function(done) {
+	exec('git checkout --orphan gh-pages', {cwd: process.cwd()},
+		function (error) {
+			if (error !== null) {
+				console.log('exec error: ' + error);
+			}
+		});
 
 	done();
 });
 
-gulp.task('git-commit-gh-pages', function(done) {
-	gulp.src(['.', '!node_modules/'])
+gulp.task('git-remove-files', ['git-create-gh-pages'], function() {
+	return gulp.src(['./**/*', '!./gulpfile.js', '!./index.html'], {'nonegate': false})
+		.pipe(git.rm({args: '-rf'}))
+		.on('error', gutil.log);
+});
+
+gulp.task('git-create-dummy-index', ['git-remove-files'], function(done) {
+	fs.writeFile('index.html', 'Docs coming soon.');
+});
+
+gulp.task('git-commit-gh-pages', ['git-create-dummy-index'], function(done) {
+	gulp.src(['./**/*', '!./node_modules/**/*.*', '!./gulpfile.js'])
+		.pipe(debug())
+		.pipe(git.checkout('gh-pages'))
 		.pipe(cache('gh-pages'))
 		.pipe(git.add())
 		.pipe(git.commit('initial gh-pages commit'))
-		.on('end', done);
+		.on('error', gutil.log);
 
 	git.push('origin', 'gh-pages');
+
+	done();
 });
 
 gulp.task('gh-pages', [
-	'init', 
-	'git-create-gh-pages',
 	'git-commit-gh-pages'
 ]);
 
@@ -95,27 +87,43 @@ gulp.task('git-add-remote', ['git-init'], function(done) {
 
 gulp.task('git-create-development-branch', ['git-init'], function(done) {
 	git.branch('development', {}, function() {});
-	gulp.src('.')
+	gulp.src('./**/*')
 		.pipe(git.checkout('development'))
+		.on('error', gutil.log)
 		.on('end', done);
 });
 
-gulp.task('setup', ['init', 'gh-pages', 'git-add-remote', 'git-create-development-branch']);
+gulp.task('setup', [
+	'gh-pages', 
+	'git-add-remote', 
+	'git-create-development-branch'
+]);
 
-// gulp.task('docs', function() {
 
-// 	var timeStamp = Date.now().toDateString();
 
-// 	gulp.src('./lib/**/*.js')
-// 		.pipe(docco())
-// 		.pipe(cache('docs'));
+gulp.task('docs', function() {
 
-// 	gulp.src('./*')
-// 		.pipe(git.checkout('gh-pages'))
-// 		.pipe(remember('docs'))
-// 		.pipe(gulp.dest('./'))
-// 		.pipe(git.commit('update docs' + timeStamp, {args: '-a -m'}))
-// 		.pipe(git.push('origin', 'gh-pages'))
-// 		.pipe(git.checkout('master'));
+	var timeStamp = Date.now().toDateString();
 
-// });
+	gulp.src('./lib/**/*.js')
+		.pipe(docco())
+		.pipe(cache('docs'));
+
+	gulp.src('./**/*')
+		.pipe(git.checkout('gh-pages'))
+		.pipe(remember('docs'))
+		.pipe(gulp.dest('./'))
+		.pipe(git.commit('update docs' + timeStamp))
+		.on('error', gutil.log);
+
+	git.push('origin', 'gh-pages');
+	
+	gulp.src('./**/*')
+		.pipe(git.checkout('master'))
+		.on('error', gutil.log);
+
+	done();
+
+});
+
+
