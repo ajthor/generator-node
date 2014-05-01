f0c7f0cfda407c3a570e155ea867878f74c10ea0
Adam <adam.thorpe.g@gmail.com>
2014-05-01 13:04:31 -0600

    add comments, clean deps

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -1,22 +1,21 @@
-var fs = require('fs');
 var exec = require('child_process').exec;
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-
 var git = require('gulp-git');
-var github = require('github');
-
 var docco = require('gulp-docco');
 
 var cache = require('gulp-cached');
 var remember = require('gulp-remember');
 
-
-gulp.task('init', ['setup']);
-
-
-
+// Init Task
+// =========
+// The `init` task does what it says, it initializes the local git 
+// repository by committing the contents of the `lib` directory as 
+// well as some of the common files that are in the root directory.
+// 
+// Usage: `gulp init`
+// 
 gulp.task('git-init', function(done) {
 	git.init();
 	done();
@@ -31,7 +30,6 @@ gulp.task('git-initial-commit', ['git-init'], function() {
 		'gulpfile.js',
 		'package.json',
 	], {dot: true})
-		.pipe(cache('master'))
 		.pipe(git.add())
 		.pipe(git.commit('initial commit'))
 		.on('error', gutil.log);
@@ -44,7 +42,7 @@ gulp.task('git-add-remote', ['git-initial-commit'], function(done) {
 	done();
 });
 
-gulp.task('setup', ['git-add-remote'], function() {
+gulp.task('init', ['git-add-remote'], function() {
 	git.push('origin', 'master');
 });
 
@@ -53,8 +51,15 @@ gulp.task('git-checkout-master', function() {
 		.pipe(git.checkout('master'));
 });
 
-
-
+// Gh-Pages Task
+// =============
+// The `gh-pages` task creates an orphaned branch and removes all 
+// files except for index.html, which it commits to the branch as the 
+// only file. This file is intended to be used as the repo's 
+// page on GitHub. __NOTE:__ Very buggy currently.
+// 
+// Usage: `gulp gh-pages`
+// 
 gulp.task('git-create-gh-pages', ['git-init'], function(done) {
 	exec('git checkout --orphan gh-pages', {cwd: process.cwd()},
 		function (error) {
@@ -80,7 +85,6 @@ gulp.task('git-remove-files', ['git-create-gh-pages'], function() {
 
 gulp.task('git-commit-gh-pages', ['git-remove-files'], function() {
 	return gulp.src(['index.html'])
-		.pipe(cache('gh-pages'))
 		.pipe(git.add())
 		.pipe(git.commit('initial gh-pages commit'))
 		.on('error', gutil.log);
@@ -90,8 +94,11 @@ gulp.task('gh-pages', ['git-commit-gh-pages'], function() {
 	git.push('origin', 'gh-pages');
 });
 
-
-
+// Dev Task
+// ========
+// The `dev` task basically creates a 'development' branch off of 
+// master and switches to it. Make sure to call this after calling 
+// `gulp init`.
 gulp.task('git-create-development-branch', ['git-checkout-master'], function() {
 	git.branch('development', {}, function() {});
 	
@@ -102,8 +109,11 @@ gulp.task('git-create-development-branch', ['git-checkout-master'], function() {
 
 gulp.task('dev', ['git-create-development-branch']);
 
-
-
+// Docs Task (untested)
+// ====================
+// I would like for the docs task to run docco, switch to the 
+// gh-pages branch, commit the files to the gh-pages branch, and 
+// switch back to master. Haven't tried it yet.
 gulp.task('docs', ['git-checkout-master'], function(done) {
 
 	var timeStamp = Date.now().toDateString();
